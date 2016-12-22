48887a6a44c44c8e39cd56e4ebe4b81617d36be8
iceberg3000 <175568505@qq.com>
2016-12-22 23:25:19 +0800

    add private

--- project4/Employee.class.php
+++ project4/Employee.class.php
@@ -1,7 +1,7 @@
 <?php
 class Employee{
-    public $name;
-    public $age;
+    private $name;
+    private $age;
     public function introduce(){
         echo '大家好，我叫'.$this->name.',今年'.$this->age.'岁!<br/>';
     }
